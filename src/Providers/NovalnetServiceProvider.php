57f1a80afb7f9256c78e8b0a15954f7b22983b47
kotteeswaranv <kotteeswaran@rootsbridge.com>
2018-01-02 12:47:58 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -182,11 +182,13 @@ class NovalnetServiceProvider extends ServiceProvider
                             $contentType = 'continue';
                             
                         } else if (in_array($paymentKey, ['NOVALNET_SEPA', 'NOVALNET_CC']))
-                        {                              
+                        {   
+                            $paymentProcessUrl = $paymentService->getProcessPaymentUrl();
                             $encodedKey = base64_encode(trim($paymentHelper->getNovalnetConfig('activation_key')) . '&' . $paymentHelper->getRemoteAddress() . '&' . $paymentHelper->getServerAddress());
                             $nnIframeSource = 'https://secure.novalnet.de/cc?signature=' . $encodedKey . '&ln=' . $sessionStorage->getLocaleSettings()->language;
                             $content = $twig->render('Novalnet::PaymentForm.Cc', [
-                                                                'nnCcFormUrl' => $nnIframeSource
+                                                                'nnCcFormUrl' => $nnIframeSource,
+                                                                'nnPaymentProcessUrl' => $paymentProcessUrl
                                    ]);
 
                             $contentType = 'htmlContent';
