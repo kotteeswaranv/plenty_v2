a8722828f46cdf2297392b42b7769a484f57c330
kotteeswaranv <kotteeswaran@rootsbridge.com>
2018-01-08 12:55:33 +0530

    Update PaymentController.php

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -142,6 +142,7 @@ class PaymentController extends Controller
         {
             $serverRequestData['data']['sepa_hash'] = $requestData['nn_sepa_hash'];
             $serverRequestData['data']['sepa_unique_id'] = $requestData['nn_sepa_uniqueid'];
+            $serverRequestData['data']['bank_account_holder'] = $requestData['sepa_cardholder'];
         } 
         $this->getLogger(__METHOD__)->error('serverRequestData', $serverRequestData);
         $response = $this->paymentHelper->executeCurl($serverRequestData['data'], $serverRequestData['url']);
