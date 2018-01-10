67c712a07bc2e41db7d842b21c1a970b9f810ff7
kotteeswaranv <kotteeswaran@rootsbridge.com>
2018-01-10 18:02:19 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -189,13 +189,16 @@ class NovalnetServiceProvider extends ServiceProvider
 				$nnDetails['sepaacceptmandate'] = $paymentHelper->getTranslatedText('sepaacceptmandate');
 				$nnDetails['sepacountryerror'] = $paymentHelper->getTranslatedText('sepacountryerror');
 				$nnCountryList = $paymentHelper->getCountryList($sessionStorage->getLocaleSettings()->language);
+				$guaranteeStatus = getGuaranteeStatus($basketRepository->load(), $paymentKey);    
 				$this->getLogger(__METHOD__)->error('nncheckcountries', $nnCountryList);
                                 $content = $twig->render('Novalnet::PaymentForm.Sepa', [
                                                                     'nnPaymentProcessUrl' => $paymentProcessUrl,
                                                                     'paymentMopKey'     =>  $paymentKey,
 								    'nnSepaHiddenValue' => $nnDetails,
+								    'nnGuaranteeStatus' => $guaranteeStatus,
 																	'nnCountry' =>$nnCountryList
                                        ]);
+				$contentType = 'htmlContent';
                             }
                             else 
                             {
@@ -209,8 +212,9 @@ class NovalnetServiceProvider extends ServiceProvider
                                                                     'paymentMopKey'     =>  $paymentKey,
 								    'nnFormDesign'	=>  $nnFormDesign
                                        ]);
+				$contentType = 'htmlContent';
                             }
-                            $contentType = 'htmlContent';
+                            
                         } 
                         else
                         {
