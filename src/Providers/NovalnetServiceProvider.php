86ddbf860ab3b2c7390fae48a71047043a937ea8
kotteeswaranv <kotteeswaran@rootsbridge.com>
2018-01-10 12:31:05 +0530

    Update NovalnetServiceProvider.php

--- src/Providers/NovalnetServiceProvider.php
+++ src/Providers/NovalnetServiceProvider.php
@@ -163,8 +163,8 @@ class NovalnetServiceProvider extends ServiceProvider
                     $this->getLogger(__METHOD__)->error('TEST', $event->getMop());
                     if($paymentHelper->isNovalnetPaymentMethod($event->getMop()))
                     {
-		       $this->getLogger(__METHOD__)->error('GURANTEESTATUS', $paymentService->getGuaranteeStatus($basketRepository->load(), $paymentKey));
-                        $paymentKey = $paymentHelper->getPaymentKeyByMop($event->getMop());
+		     $paymentKey = $paymentHelper->getPaymentKeyByMop($event->getMop());
+		       $this->getLogger(__METHOD__)->error('GURANTEESTATUS', $paymentService->getGuaranteeStatus($basketRepository->load(), $paymentKey));                       
                         if(in_array($paymentKey, ['NOVALNET_INVOICE', 'NOVALNET_PREPAYMENT', 'NOVALNET_CASHPAYMENT']))
                         {
                             $serverRequestData = $paymentService->getRequestParameters($basketRepository->load(), $paymentKey); 
